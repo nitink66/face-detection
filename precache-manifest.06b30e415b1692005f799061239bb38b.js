5cf99fad8d2ba8e1a2ebd9742be8d6ae68fda447
nitink66 <60770391+nitink66@users.noreply.github.com>
2020-03-25 20:29:15 +0530

    Updates

---
+++ precache-manifest.06b30e415b1692005f799061239bb38b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57eb0b5f5c10b9a7b13c85d986670a7",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "966accae0e26777363c3",
+    "url": "/face-detection/static/css/main.d5f7b5d5.chunk.css"
+  },
+  {
+    "revision": "af7d443290df5dffe4b6",
+    "url": "/face-detection/static/js/2.7f2cee47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-detection/static/js/2.7f2cee47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966accae0e26777363c3",
+    "url": "/face-detection/static/js/main.dd89c6b8.chunk.js"
+  },
+  {
+    "revision": "c9b78fd7059d764ed646",
+    "url": "/face-detection/static/js/runtime-main.7a455d55.js"
+  }
+]);
