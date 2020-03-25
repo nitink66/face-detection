ac61894e72b43d626c15f41e53212a10480f594d
nitink66 <60770391+nitink66@users.noreply.github.com>
2020-03-25 20:11:03 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/Signin';
+import Register from './components/Register/Register';
 import Clarifai from 'clarifai';
 import './App.css';
 
@@ -78,25 +80,26 @@ class App extends Component{
     this.state= {
       input:'',
       imageUrl:'',
-      box:{}
+      box:{},
+      route :'signin',
+      isSignedIn : false
     }
   }
 
     calculateFaceLocation = (data) =>{
-      const claraifaiface= data.outputs[0].data.regions[0].region_info.bounding_box;
+      const clarifaiFace= data.outputs[0].data.regions[0].region_info.bounding_box;
       const image = document.getElementById('inputImage');
       const width = Number(image.width);
       const height = Number(image.height);
       return {
-        leftCol : claraifaiface.left_col * width ,
-        topRow : claraifaiface.top_row *height ,
-        rightCol : width -(claraifaiface.right_col * width),
-        bottomRow :height -(claraifaiface.bottom_row * height)
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+       bottomRow: height - (clarifaiFace.bottom_row * height)
       }
     }
 
     displayFaceBox = (box) =>{
-     
       this.setState({ box :box});
     }
 
@@ -115,19 +118,42 @@ class App extends Component{
     .catch(err => console.log(err));
   }
 
+    onRouteChange = (route) => {
+      if(route === 'signout'){
+          this.setState({ isSignedIn : false})
+      }
+      else if(route ==='home'){
+        this.setState({ isSignedIn : true})
+      }
+      this.setState({route : route})
+    }
+
   render(){
+   const { isSignedIn, imageUrl, route, box } = this.state;
     return(
       <div className="App">
       <Particles className="particles"
          params = { ParticleOptions}
          />
-         <Navigation />
-         <Logo />
+         <Navigation isSignedIn={isSignedIn} onRouteChange= {this.onRouteChange}/>
+         {
+           route ==='home'
+           ? 
+           <div><Logo />
          <Rank />
          <ImageLinkForm onInputChange= { this.onInputChange }
            onButtonSubmit = {this.onButtonSubmit}
          />
-          <FaceRecognition box={this.state.box} imageUrl = { this.state.imageUrl} />
+          <FaceRecognition box={box} imageUrl = { imageUrl} />
+            </div>
+           :(
+             route === 'signin' 
+             ? <SignIn onRouteChange={this.onRouteChange}/>
+             : <Register onRouteChange={this.onRouteChange}/>
+           )
+           
+           
+          }
     </div>
   );
   
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const Navigation = () =>{
-    return (
+const Navigation = ({onRouteChange, isSignedIn}) =>{
+        if(isSignedIn){
+            return (
         <nav style={{ display:"flex",justifyContent:"flex-end"}}>
-            <p className="white f3 link dim black underline pa3 pointer">SIGN OUT</p>
-        </nav>
-    );
+            <p 
+             onClick={() => onRouteChange('signin')}
+            className="white f3 link dim black underline pa3 pointer">SIGN OUT</p>
+        </nav>)
+        }else
+        {
+            return(
+            <nav style={{ display:"flex",justifyContent:"flex-end"}}>
+            <p 
+             onClick={() => onRouteChange('signin')}
+            className="white f3 link dim black underline pa3 pointer">SIGN IN</p>
+        <p 
+             onClick={() => onRouteChange('reigster')}
+            className="white f3 link dim black underline pa3 pointer">Register</p>
+        </nav>)
+        }
+    
 }
 
 export default Navigation;
